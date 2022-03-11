1d8cc63d5538ad9106e68c3bb57a614f838182c8
vathsalav <vathsala.v@gmail.com>
2022-03-11 15:05:29 -0500

    first P0 commit

---
+++ entities/account_class.py
@@ -0,0 +1,8 @@
+
+
+class Account:
+    def __init__(self,balance, customer_id, account_id):
+        self.balance = balance
+        self.customer_id = customer_id
+        self.account_id = account_id
+
---
+++ entities/customer_class_info.py
@@ -0,0 +1,7 @@
+
+
+class Customer:
+    def __init__(self, customer_id, first_name, last_name):
+        self.customer_id = customer_id
+        self.first_name = first_name
+        self.last_name = last_name
