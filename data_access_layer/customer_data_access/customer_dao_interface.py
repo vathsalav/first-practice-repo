a6acd7d8951ebd064220a0d7d11e9a2abd074a9f
vathsalav <vathsala.v@gmail.com>
2022-03-11 15:52:52 -0500

    created Interface

---
+++ data_access_layer/customer_data_access/customer_dao_interface.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+from entities.customer_class_info import Customer
+
+
+class CustomerDAOInterface(ABC):
+
+    @abstractmethod
+    def insert_into_customer_table(self, customer_obj: Customer) -> Customer:
+        pass
+
+    @abstractmethod
+    def delete_from_customer_table(self, customer_id: int) -> bool:
+        pass
+
+
+
